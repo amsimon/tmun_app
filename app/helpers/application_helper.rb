118563b52278e949d80ef71974cca80ae5b25481
Andy Simon <asimon@ucsd.edu>
2014-10-05 18:52:38 -0700

    dgfsdf:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "UCSD MUN"
+    base_title = "Model United Nations at UCSD"
     if page_title.empty?
       base_title
     else
